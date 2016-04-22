83992120b2e38124a8bb3fa0b897fe0982302ceb
Travis Rhoden <trhoden@gmail.com>
2016-04-22 13:41:05 -0700

    Add top-level client constructor
    Since gorackhd is vendoring go-swagger (which pulls in go-openapi),
    conflicts arise if gorackhd clients are forced to import go-openapi as
    well. Fix that by creating a top-level constructure, which will
    automatically use the vendored versions of the go-openapi library.

--- gorackhd.go
+++ gorackhd.go
@@ -5,6 +5,13 @@ import (
 	_ "github.com/go-swagger/go-swagger"
 
 	// import the generated go-swagger bindings
-	_ "github.com/emccode/gorackhd/client"
+	"github.com/emccode/gorackhd/client"
 	_ "github.com/emccode/gorackhd/models"
+
+	httptransport "github.com/go-openapi/runtime/client"
+	"github.com/go-openapi/strfmt"
 )
+
+func GetClient(host, basePath string, schemes []string) *client.Monorail {
+	return client.New(httptransport.New(host, basePath, schemes), strfmt.Default)
+}
